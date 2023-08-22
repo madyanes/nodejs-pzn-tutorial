72bc44e9a114d82718a449d96af82b61b5f39a85
madyanes <madyanes.dev@gmail.com>
2023-08-23 03:42:15 +0700

    Import JavaScript module (`*.js` file)

---
+++ 2-std-lib/standard-library.js
@@ -0,0 +1,10 @@
+import os from 'os'
+
+console.info(os.platform())
+console.table(os.cpus())
+
+// Warning: To load an ES module, set "type": "module" in the package.json or use the.mjs extension.
+
+// SyntaxError: Cannot use import statement outside a module
+
+// Eror akibat menggunakan ekstensi berkas '.js'
