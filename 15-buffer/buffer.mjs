7867ef5e38f10132ad4443742eb39412cd18c459
madyanes <madyanes.dev@gmail.com>
2023-08-23 14:02:26 +0700

    Buffer module intro

--- 15-buffer/buffer.mjs
+++ 15-buffer/buffer.mjs
@@ -1,11 +1,9 @@
-// NodeJS yang saya gunakan, yaitu versi 18.16.0, tidak ada kelas Buffer
+import { Buffer } from 'buffer'
 
-// Kode di bawah menggunakan cara terbaru, dengan algoritma yang sama `Uint8Array` di balik layar
+const buffer = Buffer.from('Madyan Eka Septian', 'utf8')
 
-const encoder = new TextEncoder()
-const binaryDataA = encoder.encode('Madyan Eka Septian')
-console.info(binaryDataA)
+console.info(buffer)
+console.info(buffer.toString())
 
-const decoder = new TextDecoder()
-const binaryDataB = decoder.decode(binaryDataA)
-console.info(binaryDataB)
+buffer.reverse()
+console.info(buffer.toString())
